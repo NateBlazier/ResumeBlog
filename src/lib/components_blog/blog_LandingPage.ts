c9d345783176b5e9a532c052f4483d428c042eac
Nate Blazier <nblazier@gmail.com>
2023-11-18 18:07:35 -0600

    Update blog_LandingPage.ts

--- src/lib/components_blog/blog_LandingPage.ts
+++ src/lib/components_blog/blog_LandingPage.ts
@@ -30,23 +30,15 @@ export const blogLandingPageData: BlogLandingPageData = {
 			tags: ['selenium', 'java', 'Eclipse', 'git', 'TestNG'],
 			postTime: 'Jan 22, 2019, 4:11:58 PM'
 		},
-		{
-			title: 'Java Projects',
-			imagePath: 'https://wallpapercave.com/wp/wp7250087.jpg',
-			link: '',
-			readTime: '20 min',
-			content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, aut aliquid libero ad ipsa quo fugit inventore unde. Consequuntur perferendis aperiam corporis quasi, quos exercitationem aut fuga tempore dolorum porro.`,
-			tags: ['eee', 'ee'],
-			postTime: '3/7/2022, 4:11:58 PM'
-		},
+
 		{
 			title: 'Playwright',
 			imagePath: 'https://miro.medium.com/max/1400/1*wrRk5Ut1L7mmbaFLGELcIw.png',
-			link: '/',
+			link: 'https://www.youtube.com/watch?v=dz07r-SaTWo&list=PL_VFqfaSA4UlNiN_xbxBjgSvBTNda27oW',
 			readTime: '20 min',
-			content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, aut aliquid libero ad ipsa quo fugit inventore unde. Consequuntur perferendis aperiam corporis quasi, quos exercitationem aut fuga tempore dolorum porro.`,
-			tags: ['eee', 'ee'],
-			postTime: '3/7/2022, 4:11:58 PM'
+			content: `Youtube playlist to get started on playwright... read the documentation.`,
+			tags: ['NodeJS', 'Playwright'],
+			postTime: 'Jul 8, 2021, 11:11:23 PM'
 		},
 		{
 			title: 'git and github',
@@ -68,12 +60,12 @@ export const blogLandingPageData: BlogLandingPageData = {
 		},
 		{
 			title: 'Powershell',
-			imagePath: 'https://d1rytvr7gmk1sx.cloudfront.net/wp-content/uploads/2020/09/powershell.jpg',
+			imagePath: 'https://www.techrepublic.com/wp-content/uploads/2020/09/powershell.jpg',
 			link: '/blog/PowerShell',
 			readTime: '20 min',
-			content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, aut aliquid libero ad ipsa quo fugit inventore unde. Consequuntur perferendis aperiam corporis quasi, quos exercitationem aut fuga tempore dolorum porro.`,
-			tags: ['eee', 'ee'],
-			postTime: '3/7/2022, 4:11:58 PM'
+			content: `Powershell notes.`,
+			tags: ['Powershell'],
+			postTime: '3/7/2018, 4:11:58 PM'
 		},
 		{
 			title: 'Java Projects',
